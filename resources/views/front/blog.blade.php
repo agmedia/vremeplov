e782489bce65cbcda230a6d4973cdc5fe7979399
tomashina <tomislav@agmedia.hr>
2023-12-14 14:18:20 +0100

    blog page

--- resources/views/front/blog.blade.php
+++ resources/views/front/blog.blade.php
@@ -12,10 +12,10 @@
 
     <!-- Page Title-->
     <div class="bg-light pt-4 pb-3"  style="background-image: url({{ config('settings.images_domain') . 'media/img/vintage-bg.jpg' }});background-repeat: repeat;">
-        <div class="container d-lg-flex justify-content-between py-2 py-lg-3">
+        <div class="container  justify-content-between py-2 py-lg-3">
             <div class="order-lg-2 mb-3 mb-lg-0 pt-lg-2">
                 <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb breadcrumb-dark flex-lg-nowrap justify-content-center justify-content-lg-start">
+                    <ol class="breadcrumb breadcrumb-dark justify-content-center ">
                                 <li class="breadcrumb-item"><a class="text-nowrap" href="{{ route('index') }}"><i class="ci-home"></i>Naslovnica</a></li>
                                 <li class="breadcrumb-item"><a class="text-nowrap" href="{{ route('catalog.route.blog') }}"><i class="ci-home"></i>Blog</a></li>
 
