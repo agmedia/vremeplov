672c69486f1cf2f73915ef431cc652386b65586e
tomashina <tomislav@agmedia.hr>
2023-12-15 14:20:03 +0100

    title category home

--- resources/views/front/catalog/category/index.blade.php
+++ resources/views/front/catalog/category/index.blade.php
@@ -69,6 +69,25 @@
 
                 </div>
 
+            @else
+
+                <div class="order-lg-2 mb-3 mb-lg-0 pt-lg-2">
+                    <nav aria-label="breadcrumb">
+                        <ol class="breadcrumb breadcrumb-dark flex-lg-nowrap justify-content-center ">
+                            <li class="breadcrumb-item"><a class="text-nowrap" href="{{ route('index') }}"><i class="ci-home"></i>Naslovnica</a></li>
+                                <li class="breadcrumb-item text-nowrap active" aria-current="page">Web Shop</li>
+
+                        </ol>
+                    </nav>
+                </div>
+
+                <div class="order-lg-1 pe-lg-4 text-center ">
+
+                        <h1 class="h3 text-dark mb-0">Web Shop</h1>
+
+
+                </div>
+
             @endif
 
             @if (Route::currentRouteName() == 'pretrazi')
