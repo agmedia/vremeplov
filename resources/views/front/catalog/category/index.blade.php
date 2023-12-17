3d602cf633b4bc416f6bf72c09743b4fb0563c01
tomashina <tomislav@agmedia.hr>
2023-12-17 15:47:59 +0100

    breadcrums fix dark

--- resources/views/front/catalog/category/index.blade.php
+++ resources/views/front/catalog/category/index.blade.php
@@ -99,7 +99,7 @@
 
             @if (Route::currentRouteName() == 'pretrazi')
                 <div class="order-lg-1 pe-lg-4 text-center text-lg-start">
-                    <h1 class="h3 text-light mb-0"><span class="small fw-light me-2">Rezultati za:</span> {{ request()->input('pojam') }}</h1>
+                    <h1 class="h3 text-dark mb-0"><span class="small fw-light me-2">Rezultati za:</span> {{ request()->input('pojam') }}</h1>
                 </div>
             @endif
 
@@ -131,7 +131,7 @@
             @if (isset($publisher) && $publisher)
                 <div class="order-lg-2 mb-3 mb-lg-0 pt-lg-2">
                     <nav aria-label="breadcrumb">
-                        <ol class="breadcrumb breadcrumb-light flex-lg-nowrap justify-content-center justify-content-lg-start">
+                        <ol class="breadcrumb breadcrumb-dark flex-lg-nowrap justify-content-center justify-content-lg-start">
                             <li class="breadcrumb-item"><a class="text-nowrap" href="{{ route('index') }}"><i class="ci-home"></i>Naslovnica</a></li>
                             <li class="breadcrumb-item text-nowrap active" aria-current="page"><a class="text-nowrap" href="{{ route('catalog.route.publisher') }}">Nakladnici</a></li>
                             @if ( ! $cat && ! $subcat)
@@ -149,7 +149,7 @@
                     </nav>
                 </div>
                 <div class="order-lg-1 pe-lg-4 text-center text-lg-start">
-                    <h1 class="h3 text-light mb-0">{{ $publisher->title }}</h1>
+                    <h1 class="h3 text-dark mb-0">{{ $publisher->title }}</h1>
                 </div>
             @endif
 
