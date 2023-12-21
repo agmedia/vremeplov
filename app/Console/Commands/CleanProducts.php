a196b815435ff38743557dfc07fdb30c3fe3d91b
Filip Jankoski <filip@agmedia.hr>
2023-12-21 12:53:48 +0100

    _add: Clean products cron. temp.

---
+++ app/Console/Commands/CleanProducts.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Helpers\ProductHelper;
+use App\Http\Controllers\Back\DashboardController;
+use App\Models\Back\Catalog\Product\Product;
+use Illuminate\Console\Command;
+use Illuminate\Http\Request;
+
+class CleanProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'clean:products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clean & resolve all products groups, categories and url.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $import = new OC_Import();
+        $range = $import->resolveProductsImportRange()->first();
+        $products = Product::query()->offset($range->offset)->take($range->limit)->with('categories')->get();
+
+        foreach ($products as $item) {
+            if ($item->group && isset($item->categories()->first()->group)) {
+                $item->update([
+                    'url'             => ProductHelper::url($item),
+                    'category_string' => ProductHelper::categoryString($item)
+                ]);
+
+            } else {
+                $cats = $import->getProductCategories($item->ean);
+
+                if ($cats->count()) {
+                    $path = $import->getCategoryPath($cats->first()->category_id);
+
+                    if (isset($path->keyword)) {
+                        $item->update([
+                            'group' => $path->keyword
+                        ]);
+                    }
+                }
+            }
+        }
+
+        $import->resolveProductsImportRange(($range->offset + $range->limit), $range->limit);
+
+        return response()->json(['success' => 'Import je uspješno obavljen..!']);
+    }
+}
---
+++ database/001_add_group_to_products_table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE `products`
+    ADD COLUMN `group` VARCHAR(191) NULL DEFAULT NULL AFTER `description`;
