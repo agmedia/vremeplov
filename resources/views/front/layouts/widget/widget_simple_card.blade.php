f25c76d17cba6cbf012f054e0e5b81bc2a4669b0
tomashina <tomislav@agmedia.hr>
2023-12-22 14:01:38 +0100

    fix accesibility widgets

--- resources/views/front/layouts/widget/widget_simple_card.blade.php
+++ resources/views/front/layouts/widget/widget_simple_card.blade.php
@@ -10,7 +10,7 @@
                 <div class="d-block d-sm-flex justify-content-between align-items-center  rounded-3" style="background-image: url({{ config('settings.images_domain') . 'media/img/vintage-bg.jpg' }});background-repeat: repeat;">
                         <div class="pt-5 py-sm-4 px-4 ps-md-4 pe-4 text-center text-sm-start pb-4 pb-md-0">
                                 <h2 class="font-title">{{ $widget['title'] }}</h2>
-                                <p class="text-muted pb-2">{{ $widget['subtitle'] }}</p><a class="btn btn-primary" href="{{ url($widget['url']) }}">Pogledajte ponudu <i class="ci-arrow-right ms-2 me-n1"></i></a>
+                                <p class="text-muted pb-2">{{ $widget['subtitle'] }}</p><a class="btn btn-primary" aria-label="Pogledajte ponudu" href="{{ url($widget['url']) }}">Pogledajte ponudu <i class="ci-arrow-right ms-2 me-n1"></i></a>
                         </div>
                        <img class="d-block mx-auto mx-sm-0 rounded-end pb-4 pb-sm-0 " src="{{ $widget['image'] }}" style="max-width:290px" alt="{{ $widget['title'] }}">
                 </div>
