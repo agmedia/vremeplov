d2c8f94af5c975f56a40b5274486445dffc15bc0
tomashina <tomislav@agmedia.hr>
2023-12-22 14:13:48 +0100

    remove log

--- resources/js/front/cart/components/CartNavIcon/CartNavIcon.vue
+++ resources/js/front/cart/components/CartNavIcon/CartNavIcon.vue
@@ -71,7 +71,7 @@
                 let kos = [];
                 let cart = this.$store.state.storage.getCart();
 
-                console.log(cart)
+
 
                 this.$store.dispatch('getSettings');
 
