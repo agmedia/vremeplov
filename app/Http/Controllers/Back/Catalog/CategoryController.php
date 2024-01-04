c24f507f645804a01b5888072bab5e01b5d20d85
Filip Jankoski <filip@agmedia.hr>
2024-01-04 18:51:46 +0100

    _fix: Category create.

--- app/Http/Controllers/Back/Catalog/CategoryController.php
+++ app/Http/Controllers/Back/Catalog/CategoryController.php
@@ -32,7 +32,7 @@ class CategoryController extends Controller
      */
     public function create()
     {
-        $groups = Category::groups()->pluck('group');
+        $groups = \App\Models\Front\Catalog\Category::getGroups();
         $parents = Category::topList()->pluck('title', 'id');
 
         return view('back.catalog.category.edit', compact('parents', 'groups'));
