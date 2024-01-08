ef02b103da73af323e5524deb002198c74190793
tomashina <tomislav@agmedia.hr>
2024-01-08 09:25:50 +0100

    footer fix

--- resources/views/front/layouts/partials/footer.blade.php
+++ resources/views/front/layouts/partials/footer.blade.php
@@ -102,7 +102,7 @@
                         <li class="widget-list-item"><a href="info/nacini-placanja" class="widget-list-link" > kreditnom karticom jednokratno ili na rate</a></li>
                         <li class="widget-list-item"><a href="info/nacini-placanja" class="widget-list-link" > virmanom / općom uplatnicom / internet bankarstvom</a></li>
                         <li class="widget-list-item"><a href="info/nacini-placanja" class="widget-list-link" >gotovinom prilikom pouzeća</a></li>
-                        <li class="widget-list-item"><a href="info/nacini-placanja" class="widget-list-link" >paypal-om</a></li>
+                       <!-- <li class="widget-list-item"><a href="info/nacini-placanja" class="widget-list-link" >paypal-om</a></li>-->
                         <li class="widget-list-item"><a href="info/nacini-placanja" class="widget-list-link" >osobno preuzimanje i plaćanje u antikvarijatu</a></li>
                     </ul>
 
