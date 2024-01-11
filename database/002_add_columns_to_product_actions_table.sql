62db8633abfe2ba5d0cd678aadb267969bcada52
Filip Jankoski <filip@agmedia.hr>
2024-01-11 10:27:45 +0100

    _fix: Actions table and group list.

--- database/002_add_columns_to_product_actions_table.sql
+++ database/002_add_columns_to_product_actions_table.sql
@@ -1,2 +1,5 @@
 ALTER TABLE `product_actions`
     ADD COLUMN `quantity` INT(10) UNSIGNED NOT NULL DEFAULT 0 AFTER `logged`;
+
+ALTER TABLE `product_actions`
+    ADD COLUMN `coupon` VARCHAR(15) NULL DEFAULT NULL AFTER `badge`;
