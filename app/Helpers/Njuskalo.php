0b4aa4bc3e7ef0d21d989b376c981059954a597c
Filip Jankoski <filip@agmedia.hr>
2024-01-22 12:03:34 +0100

    _fix: Description on Njuskalo export.

--- app/Helpers/Njuskalo.php
+++ app/Helpers/Njuskalo.php
@@ -10,22 +10,27 @@ use App\Models\Back\Catalog\Product\Product;
 class Njuskalo
 {
 
+    /**
+     * @var array 
+     */
     private $response = [];
 
 
+    /**
+     * @return array
+     */
     public function getItems(): array
     {
         $products = Product::query()->where('status', 1)
                                     ->where('price', '!=', 0)
                                     ->where('quantity', '!=', 0)
-                                    ->select('id', 'name', 'price', 'group', 'description', 'image')
                                     ->get();
 
         foreach ($products as $product) {
             $this->response[] = [
                 'id' => $product->id,
                 'name' => $product->name,
-                'description' => $product->description ?: $product->name,
+                'description' => $this->getDescription($product),
                 'group' => config('settings.njuskalo.sync.' . $product->group),
                 'price' => $product->price,
                 'image' => asset($product->image),
@@ -33,5 +38,43 @@ class Njuskalo
         }
 
         return $this->response;
+    }
+
+
+    /**
+     * @param Product $product
+     *
+     * @return string
+     */
+    private function getDescription(Product $product): string
+    {
+        $str = '';
+
+        if ($product->description != '') {
+            $str .= $product->description . '<br><br>';
+        }
+        if ($product->pages) {
+            $str .= 'Stranica: ' . $product->pages . '<br>';
+        }
+        if ($product->dimensions) {
+            $str .= 'Dimenzije: ' . $product->dimensions . '<br>';
+        }
+        if ($product->origin) {
+            $str .= 'Jezik: ' . $product->origin . '<br>';
+        }
+        if ($product->letter) {
+            $str .= 'Pismo: ' . $product->letter . '<br>';
+        }
+        if ($product->condition) {
+            $str .= 'Stanje: ' . $product->condition . '<br>';
+        }
+        if ($product->binding) {
+            $str .= 'Uvez: ' . $product->binding . '<br>';
+        }
+        if ($product->year) {
+            $str .= 'Godina: ' . $product->year . '<br>';
+        }
+
+        return $str;
     }
 }
