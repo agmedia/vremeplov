57681f334737d8bb30ab01240fb2a1e0552aa9a7
tomashina <tomislav@agmedia.hr>
2024-03-25 23:11:30 +0100

    fix cart

--- resources/js/front/cart/bootstrap.js
+++ resources/js/front/cart/bootstrap.js
@@ -28,6 +28,9 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.axios.defaults.withCredentials = true;
+window.axios.defaults.withXSRFToken = true;
+
 window.axios.defaults.baseURL = API_PATH;
 
 /**
