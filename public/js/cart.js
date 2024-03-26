9101cf8873f6763280d953e4c1ab4898312b8cc4
tomashina <tomislav@agmedia.hr>
2024-03-26 10:45:06 +0100

    cors

--- public/js/cart.js
+++ public/js/cart.js
@@ -3777,7 +3777,6 @@ var API_PATH = BASE_PATH + '/api/v2/';
 
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.headers.common['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
 window.axios.defaults.baseURL = API_PATH;
 
 /**
