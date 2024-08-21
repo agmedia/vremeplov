107084e047c186bcd5bb4be88b144256c3ac45fb
tomashina <tomislav@agmedia.hr>
2024-08-21 12:23:58 +0200

    gls

--- database/004_add_columns_to_order_table.sql
+++ database/004_add_columns_to_order_table.sql
@@ -1,3 +1,2 @@
-ALTER TABLE `orders`
-    ADD COLUMN  `commentp` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci, AFTER `comment`;
+ALTER TABLE `orders` ADD COLUMN  `commentp` text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci AFTER `comment`;
 
