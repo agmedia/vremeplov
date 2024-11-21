67a7a4a8f743106f2fbd2291c404cbd3309f6d88
tomashina <tomislav@agmedia.hr>
2024-11-21 10:37:13 +0100

    widget fix

--- resources/views/front/layouts/widget/widget_simple_card.blade.php
+++ resources/views/front/layouts/widget/widget_simple_card.blade.php
@@ -6,6 +6,7 @@
 
     <div class="row  mt-1 ">
         @foreach ($data as $widget)
+            @if($widget['width'] == 6)
             <div class="col-sm-12 col-lg-{{ $widget['width'] }} mb-grid-gutter">
                 <div class="d-block d-sm-flex justify-content-between align-items-center  rounded-3" style="background-image: url({{ config('settings.images_domain') . 'media/img/vintage-bg.jpg' }});background-repeat: repeat;">
                         <div class="pt-5 py-sm-4 px-4 ps-md-4 pe-4 text-center text-sm-start pb-4 pb-md-0">
@@ -15,6 +16,16 @@
                     <a  href="{{ url($widget['url']) }}">   <img class="d-block mx-auto mx-sm-0 rounded-end pb-4 pb-sm-0 " width="290" height="290" src="{{ $widget['image'] }}" style="max-width:290px" alt="{{ $widget['title'] }}"></a>
                 </div>
             </div>
+
+            @else
+
+                <div class="col-sm-12 col-lg-{{ $widget['width'] }} mb-grid-gutter">
+                    <div class="d-block d-sm-flex justify-content-between align-items-center  rounded-3" style="background-image: url({{ config('settings.images_domain') . 'media/img/vintage-bg.jpg' }});background-repeat: repeat;">
+                        <a  href="{{ url($widget['url']) }}">   <img class="d-block mx-auto mx-sm-0 rounded-end pb-4 pb-sm-0 "  src="{{ $widget['image'] }}" style="max-width:290px" alt="{{ $widget['title'] }}"></a>
+                    </div>
+                </div>
+
+           @endif
         @endforeach
     </div>
 </section>
