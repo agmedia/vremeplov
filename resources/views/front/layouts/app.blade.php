1e1adb130a2c17684ec373c7907650f3abf1b020
tomashina <tomislav@agmedia.hr>
2026-09-10 07:33:13 +0200

    Hide contact shortcuts on mobile

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -94,7 +94,7 @@
 
     <!-- Main Theme Styles + Bootstrap-->
     <link rel="stylesheet" media="screen" href="/css/theme.css?v=1.91">
-    <link rel="stylesheet" media="screen" href="/css/front-vremeplov.css?v=1.0.45">
+    <link rel="stylesheet" media="screen" href="/css/front-vremeplov.css?v=1.0.46">
     @include('front.layouts.partials.analytics')
 
     @stack('css_after')
